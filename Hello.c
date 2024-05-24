601af06e2c58f8db973ca5fc61d60b031bc1e4ee
Ze1ex <moses49618@gmail.com>
2024-05-24 15:54:33 +0700

    first commit

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 16;
+    //Print Hello World to the screen
+    printf("hello world!\n");
+    printf("My Name is Mateo!\n");
+    printf("Age: %d\n", age);
+    return 0;
+}
